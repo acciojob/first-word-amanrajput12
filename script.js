11b0f262a1552b6e89f014caf5009d729bb7ffb8
Aman Rajput <89723728+amanrajput12@users.noreply.github.com>
2025-02-03 15:01:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function firstWord(s) {
   // your code here
     const value = s.split(" ")[0];
-		return value
+		return value+`'`
 	
 }
-
 // Do not change the code below
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
+
