21deb213c16d28f1b7a43961040347c262f9e4ec
Aman Rajput <89723728+amanrajput12@users.noreply.github.com>
2025-02-03 15:20:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 function firstWord(s) {
-  // your code here
-    const value = s.split(" ")[0];
-		return value+`'`
-	
+   // your code here
+ //    const value = s.split(" ")[0];
+	// if(value=="" || value==''){
+	// 	return value+"if"
+	// }
+	// else{
+	// return value+s[s.length-1]+"else";
+	// }
+	// return s.charCodeAt(0)
+	let value="";
+	for(let i=0;i<s.length-1;i++){
+         if(s.charCodeAt(i)==32){
+          break;
+		 }
+		else{
+            value=value+s[i];
+		}
+	}
+	return value+s[s.length-1];
 }
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
+
 
